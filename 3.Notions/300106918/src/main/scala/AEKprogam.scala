6ec12ca64b3ed86dd5b73458c90a790451549f31
unknown <you@example.com>
2019-02-08 22:38:01 -0500

    work for the worst and plan for the best

---
+++ 3.Notions/300106918/src/main/scala/AEKprogam.scala
@@ -0,0 +1,13 @@
+object AEKprogam {
+  def main(args: Array[String]): Unit = {
+  }
+       var a = 10;
+       var b = 20;
+       var c = 25;
+       var d = 25;
+    println("a + b = " + (a + b));
+    println("a - b = " + (a - b));
+    println("a * b = " + (a * b));
+    println("b / a = " + (b / a));
+    println("c % a = " + (c % a));
+}
