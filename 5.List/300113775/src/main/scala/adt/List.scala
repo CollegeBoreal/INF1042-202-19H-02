fef4aad4f7a34ff9cb35234cf267bf8461ea96a0
widby <widbyarmand@gmail.com>
2019-02-19 16:17:20 -0500

    Armand

--- 5.List/300113775/src/main/scala/adt/List.scala
+++ 5.List/300113775/src/main/scala/adt/List.scala
@@ -1,21 +1,20 @@
 package adt
 
 sealed trait List[+A]
-case object Nil extends List [Nothing]
-case class Cons[+A](head: A, tail: List[A]) extends List [A]
+case object Nil extends List[Nothing]
+case class Cons[+A](head: A, tail: List[A]) extends List[A]
 
 object List {
-  def sum(ints: List[Int]):Int = ints match {
-    case Nil          =>0
-    case Cons(x, xs)  =>x + sum(xs)
+  def sum(ints: List[Int]): Int = ints match {
+    case Nil         => 0
+    case Cons(x, xs) => x + sum(xs)
   }
   val coucou = "coucou"
 
-
   def product(ds: List[Double]): Double = ds match {
-    case Nil         => 1.0
-    case Cons(0.0, _)=> 0.0
-    case Cons(x, xs) => x*product(xs)
+    case Nil          => 1.0
+    case Cons(0.0, _) => 0.0
+    case Cons(x, xs)  => x * product(xs)
   }
 
   def apply[A](as: A*): List[A] =
