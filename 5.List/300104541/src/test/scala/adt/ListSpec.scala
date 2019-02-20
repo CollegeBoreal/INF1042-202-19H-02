69b29f6f360765de3522b79d77ff0eca0ba5b181
unknown <mianromeo10@gmail.com>
2019-02-20 17:46:46 -0500

    Romeo

--- 5.List/300104541/src/test/scala/adt/ListSpec.scala
+++ 5.List/300104541/src/test/scala/adt/ListSpec.scala
@@ -16,4 +16,11 @@ class ListSpec extends FlatSpec with Matchers {
     List.product(List(1.0, 2.0, 3.0)) shouldEqual 6.0
   }
 
+  "trouver le premier" should "afficher 3" in {
+    List.findFirst(Array("Romeo", "Halima", "David", "Djuma"), "Djuma") shouldEqual 3
+  }
+
+  "trouver algebraiquement l indice" should "afficher 2" in {
+    List.findFirst(Array(0, 2, 4, 5), (a: Int) => a == 4) shouldEqual 2
+  }
 }
