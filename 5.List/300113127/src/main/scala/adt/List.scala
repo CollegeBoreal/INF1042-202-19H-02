ea749837b00163aed827c0cd0fc3e2ba6d866063
Futureseven <victoirerandah@hotmail.com>
2019-02-20 18:15:24 -0500

    Golden

--- 5.List/300113127/src/main/scala/adt/List.scala
+++ 5.List/300113127/src/main/scala/adt/List.scala
@@ -16,7 +16,23 @@ object List {
     case Cons(0.0, _) => 0.0
     case Cons(x, xs)  => x * product(xs)
   }
+  def findFirst(ss: Array[String], key: String): Int = {
+    @annotation.tailrec
+    def loop(n: Int): Int =
+      if (n >= ss.length) -1
+      else if (ss(n) == key) n
+      else loop(n + 1)
+    loop(0)
+  }
 
+  def findFirst[A](as: Array[A], p: A => Boolean): Int = {
+    @annotation.tailrec
+    def go(n: Int): Int =
+      if (n >= as.length) -1
+      else if (p(as(n))) n
+      else go(n + 1)
+    go(0)
+  }
   def apply[A](as: A*): List[A] =
     if (as.isEmpty) {
       Nil
