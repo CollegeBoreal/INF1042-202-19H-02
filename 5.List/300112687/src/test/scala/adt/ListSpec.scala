d3afc2967caca5babc732989e7b07ad84dc57f81
jthn9022 <43135622+jthn9022@users.noreply.github.com>
2019-04-17 08:12:21 -0400

    aaa

--- 5.List/300112687/src/test/scala/adt/ListSpec.scala
+++ 5.List/300112687/src/test/scala/adt/ListSpec.scala
@@ -12,8 +12,24 @@ class ListSpec extends FlatSpec with Matchers {
     List.sum(List(1, 2, 3)) shouldEqual 6
   }
 
-  "le produit" should "afficher 6.0" in {
+  "le produit" should "afficher 0" in {
     List.product(List(1.0, 2.0, 3.0)) shouldEqual 6.0
+  }
+
+  "trouver le premier" should "afficher 3" in {
+    List.findFirst(Array("Romeo", "Halima", "David", "Djumaster"), "Djumaster") shouldEqual 3
+  }
+
+  "trouver algebraiquement l'indice" should "afficher 2" in {
+    List.findFirst(Array(0, 2, 4, 5), (a: Int) => a == 4) shouldEqual 2
+  }
+
+  "trouver l'indince d'un tableau de double" should "afficher 4" in {
+    List.findFirst(Array(1.2, 1.3, 1.6, 2.3, 9.7, 7.9), (a: Double) => a == 9.7) shouldEqual 4
+  }
+
+  "trouvre le charactere" should "afficher 3" in {
+    List.findFirst(Array('a', 'c', 'f', 'w'), (a: Char) => a == 'w') shouldEqual 3
   }
 
 }
